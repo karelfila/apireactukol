e2467280a61f2c9639e9f20f9abc94f9ce59bda9
karelfila <72249573+karelfila@users.noreply.github.com>
2024-03-25 16:32:58 +0100

    done

---
+++ src/app/[country]/page.js
@@ -0,0 +1,40 @@
+import Link from 'next/link';
+
+
+export default async function Detail({params}) {
+
+    async function getData() {
+        const res = await fetch(`https://restcountries.com/v3.1/name/${params.country}`)
+
+
+        if (!res.ok) {
+
+            throw new Error('Failed to fetch data')
+        }
+
+        console.log(res)
+        return res.json()
+    }
+
+    const data = await getData();
+
+    return (
+        <main>
+        {
+            data.map(country => (<div>
+
+                    <h1>{country.name.official}</h1>
+
+                    <img src={country.flags.png}/>
+
+                    <h2>Continent: {country.continents}</h2>
+
+                    <h2>Borders: {country.borders}</h2>
+                    <h2>Area: {country.area}</h2>
+                    <h2>Acronym: {country.cca2}</h2>
+                </div>
+            ))
+        }
+     </main>
+    );
+}
